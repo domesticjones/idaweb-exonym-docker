4929c839b5c04bcec44a728226776459cafb8383
Dustin Jones <info@domesticjones.com>
2019-11-05 21:57:19 -0700

    fix footer email signup. update software versions

--- header.php
+++ header.php
@@ -5,6 +5,14 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<title><?php wp_title(); ?></title>
 		<?php wp_head(); ?>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143790355-1"></script>
+		<script>
+			 window.dataLayer = window.dataLayer || [];
+			 function gtag(){dataLayer.push(arguments);}
+			 gtag('js', new Date());
+			 gtag('config', 'UA-143790355-1');
+		</script>
 	</head>
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 		<h1 id="site" class="header-meta"><?php echo get_bloginfo('name'); ?></h1>
